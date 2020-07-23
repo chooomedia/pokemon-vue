8356619503c8f5cc265112d7c0e7efb114837e40
chooomedia <christopher@chooomedia.de>
2020-07-23 17:05:01 +0200

    fancy styling and npm changes

--- src/components/Pokemon.vue
+++ src/components/Pokemon.vue
@@ -48,5 +48,23 @@
 </script>
 
 <style lang="scss" scoped>
+@import url('https://fonts.googleapis.com/css2?family=Nunito&display=swap';
+
+.container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    padding: 15px;
+    width: calc(100% - 30px);
+    min-height: calc(100vh - 30px);
+    background: radial-gradient(#0099ca, #000);
+
+    font-family: 'Raleway', arial, sans-serif;
+    font-size: 1rem;
+    font-weight: normal;
+  }
+
+  h1 { color: #efefef; }
 
 </style>
