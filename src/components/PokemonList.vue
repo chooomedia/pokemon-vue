e1b5f37d06ae84a2e131513d388dd7d5deffb44e
chooomedia <christopher@chooomedia.de>
2020-07-23 16:42:08 +0200

    created intial List and Search component

---
+++ src/components/PokemonList.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="list">
+    <article v-for="(pokemon, index) in pokemons"
+    :key="'poke'+index"
+    @click="setPokemonUrl(pokemon.url)">
+      <img :src="imageUrl + pokemon.id + '.png'" width="96" height="96" alt="">
+      <h3>{{ pokemon.name }}</h3>
+    </article>
+    <div id="scroll-trigger" ref="infinitescrolltrigger">
+      <i class="fas fa-spinner fa-spin"></i>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: [
+      'imageUrl',
+      'apiUrl'
+    ],
+    data: () => {
+      return {
+        pokemons: [],
+        nextUrl: '',
+        currentUrl: ''
+      }
+    },
+    methods: {
+      fetchData() {
+        let req = new Request(this.currentUrl);
+        fetch(req)
+          .then((resp) => {
+            if(resp.status === 200)
+              return resp.json();
+          })
+          .then((data) => {
+            this.nextUrl = data.next;
+            data.results.forEach(pokemon => {
+              pokemon.id = pokemon.url.split('/')
+                .filter(function(part) { return !!part }).pop();
+              this.pokemons.push(pokemon);
+            });
+          })
+          .catch((error) => {
+            console.log(error);
+          })
+      },
+      scrollTrigger() {
+        const observer = new IntersectionObserver((entries) => {
+          entries.forEach(entry => {
+            if(entry.intersectionRatio > 0 && this.nextUrl) {
+              this.next();
+            }
+          });
+        });
+
+        observer.observe(this.$refs.infinitescrolltrigger);
+      },
+      next() {
+        this.currentUrl = this.nextUrl;
+        this.fetchData();
+      },
+      setPokemonUrl(url) {
+        this.$emit('setPokemonUrl', url);
+      }
+    },
+    created() {
+      this.currentUrl = this.apiUrl;
+      this.fetchData();
+    },
+    mounted() {
+      this.scrollTrigger();
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
